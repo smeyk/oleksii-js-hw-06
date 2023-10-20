6adfeced4609017312228546a59acdf433fb2fb7
Olexii Guberniuk <oleksii@MacBook-Pro-Olexii.local>
2023-10-20 10:26:34 +0200

    add fix in 7 task

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,6 @@
 const input = document.querySelector("#font-size-control");
 const text = document.querySelector("#text");
-text.style.fontSize = "16px";
+text.style.fontSize = `${input.value}px`;
 
 const changeFontSize = (event) => {
 	input.value = event.currentTarget.value;
